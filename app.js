15fa78ef6cc18b0537db020d831a796fa1f5decf
CodeByBlazej <codebyblazej@gmail.com>
2023-12-19 22:16:06 +0000

    Finished
    I also added extra functionality to it:
    
    if statement avoiding error in case of missing name in API library
    
    I also named the function and called it at the bottom so when someone reloads page anime will always be there.

--- app.js
+++ app.js
@@ -3,46 +3,36 @@ const animeContainerEl = document.querySelector('.anime-container');
 const animeImgEl = document.querySelector('.anime-img');
 const animeNameEl = document.querySelector('.anime-name');
 
-// const apiUrl = 'https://api.waifu.im/search'
-// const params = {included_tags: 'maid', height: '>=2000'};
-// const queryParams = new URLSearchParams(params);
-// const requestUrl = `${apiUrl}?${queryParams}`;
-
-// btnEl.addEventListener('click', async function() {
-//   try {
-//     const response = await fetch(requestUrl);
-//     const data = await response.json();
-//     animeImgEl.src = data.imageURL;
-//     animeContainerEl.style.display = 'block';
-//   } catch (error) {
-    
-//   }
-// })
-
-
-const apiUrl = 'https://api.waifu.im/search';  // Replace with the actual API endpoint URL
-const params = {
-  included_tags: 'maid',
-  height: '>=2000'
-};
-
+const apiUrl = 'https://api.waifu.im/search'
+const params = {included_tags: 'maid', height: '>=2000'};
 const queryParams = new URLSearchParams(params);
 const requestUrl = `${apiUrl}?${queryParams}`;
 
-fetch(requestUrl)
-  .then(response => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error('Request failed with status code: ' + response.status);
-    }
-  })
-  .then(data => {
-    // Process the response data as needed
-    // console.log(data);
+async function getAnime() {
+  try {
+    btnEl.disabled = true;
+    btnEl.innerText = 'Loading...';
+    animeNameEl.innerText = 'Updating...';
+    animeImgEl.src = 'spinner.svg';
+    const response = await fetch(requestUrl);
+    const data = await response.json();
+    btnEl.disabled = false;
+    btnEl.innerText = 'Get Anime';
     animeContainerEl.style.display = 'block';
-    animeImgEl.src = data[1];
-  })
-  .catch(error => {
-    console.error('An error occurred:', error.message);
-  });
+    animeImgEl.src = data.images[0].url;
+    if (data.images[0].artist) {
+      animeNameEl.innerText = data.images[0].artist.name; 
+    } else {
+      animeNameEl.innerText = 'Anime name not available...'
+    }
+  } catch (error) {
+    console.log(error);
+    btnEl.disabled = true;
+    btnEl.innerText = 'Get Anime';
+    animeNameEl.innerText = 'An error happened, please try again...';
+  }
+}
+
+getAnime();
+
+btnEl.addEventListener('click', getAnime);
